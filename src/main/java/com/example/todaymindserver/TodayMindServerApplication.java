bf644a38b7f185f43fda8b16f627993c305b2283
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-02 22:45:08 +0900

    first commit

---
+++ src/main/java/com/example/todaymindserver/TodayMindServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.todaymindserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayMindServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayMindServerApplication.class, args);
+    }
+
+}
