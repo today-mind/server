8e0a601b29cdb4b449c4316f9fbfdf05078a3120
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-08 22:55:00 +0900

    refactor(oauth): User 필드 추가 - OAuth 고유 식별자인 providerUserId(sub) + provider(oauth type) 필드 추가 - email은 식별자가 아닌 단순 정보를 담는 값이므로 null 허용 제거 및 유니크 제거 - 닉네임도 계정 생성 이후에 받는 정책이므로 null 허용 제거

--- src/main/java/com/example/todaymindserver/entity/User.java
+++ src/main/java/com/example/todaymindserver/entity/User.java
@@ -1,23 +1,47 @@
 package com.example.todaymindserver.entity;
 
+import com.example.todaymindserver.common.util.OauthProviderType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    private String username;
+    private String nickName;
 
-    @Column(nullable = false)
-    private String nickname;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private OauthProviderType provider;
 
     @Column(nullable = false, unique = true)
-    private String email;
+    private String providerUserId;
+
+    private User(String email, OauthProviderType provider, String providerUserId) {
+        this.email = email;
+        this.provider = provider;
+        this.providerUserId = providerUserId;
+    }
+
+    public static User create (String email, OauthProviderType provider, String providerUserId) {
+        return new User(
+            email,
+            provider,
+            providerUserId
+        );
+    }
 }
