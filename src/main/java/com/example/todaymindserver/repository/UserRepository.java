d91c2a3442890791ac33af1d5ab30f4af040b87a
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-08 22:56:31 +0900

    feature(oauth): UserRepository 사용자 조회 JPA 메서드 추가 - provider type과 sub을 식별자로 사용

--- src/main/java/com/example/todaymindserver/repository/UserRepository.java
+++ src/main/java/com/example/todaymindserver/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.example.todaymindserver.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.todaymindserver.common.util.OauthProviderType;
 import com.example.todaymindserver.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderAndProviderUserId(OauthProviderType provider, String providerUserId);
 }
