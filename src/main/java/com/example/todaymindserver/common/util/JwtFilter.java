adc6a2797086f1b615e97d774f2e6589ac233264
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-10 14:55:01 +0900

    feature(oauth): Spring Security 기능 구현 - enum으로 서블릿 전용 에러 코드 구현 - securityResponseWriter를 통해 공통 응답 - 인증/인가 Handler, EntryPoint를 생성하여 분기 처리

---
+++ src/main/java/com/example/todaymindserver/common/util/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.example.todaymindserver.common.util;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.todaymindserver.service.JwtAuthenticationService;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtAuthenticationService authService;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+        @Nonnull HttpServletRequest request,
+        @Nonnull HttpServletResponse response,
+        @Nonnull FilterChain chain
+    ) {
+
+        try {
+            String token = resolveToken(request);
+
+            // 토큰 없으면 익명으로 진행
+            if (token != null) {
+                SecurityContextHolder.getContext().setAuthentication(
+                    authService.getAuthentication(token)
+                );
+            }
+
+            chain.doFilter(request, response);
+
+        } catch (Exception e) {
+            log.warn("JWT 검증 실패: method={}, uri={}, message={}",
+                request.getMethod(),
+                request.getRequestURI(),
+                e.getMessage()
+            );
+        }
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader("Authorization");
+
+        if(!jwtUtil.isValidBearerToken(bearer)) {
+            return null;
+        }
+
+        return jwtUtil.extractToken(bearer);
+    }
+}
