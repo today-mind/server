dacb22d6fe63fe5f332ff281fd0234089e50b94f
채우원 <chaeuwon70@gmail.com>
2025-12-12 19:33:36 +0900

    fix: JpaAuditing 설정 중복 방지를 위해 Application 클래스에서 어노테이션 제거

--- src/main/java/com/example/todaymindserver/TodayMindServerApplication.java
+++ src/main/java/com/example/todaymindserver/TodayMindServerApplication.java
@@ -2,9 +2,7 @@ package com.example.todaymindserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // 이거 남기기
 
-@EnableJpaAuditing // 이거 남기기
 @SpringBootApplication
 public class TodayMindServerApplication {
     public static void main(String[] args) {
