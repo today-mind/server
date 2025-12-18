848acc2e50b862002b016f58dac190b080f7cabc
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-18 22:00:41 +0900

    Merge pull request #13 from today-mind/feature/oauth

--- src/main/java/com/example/todaymindserver/TodayMindServerApplication.java
+++ src/main/java/com/example/todaymindserver/TodayMindServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodayMindServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TodayMindServerApplication.class, args);
     }
-
 }
