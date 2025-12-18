d0730bab7ea98fdf30526655b6ba47365579d8a0
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-18 18:44:53 +0900

    fix(ci/cd): Actuator 체크를 위해 설정 수정

--- src/main/java/com/example/todaymindserver/common/config/SecurityConfig.java
+++ src/main/java/com/example/todaymindserver/common/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/health").permitAll()
                 .requestMatchers(HttpMethod.POST,
                     "/oauth/**/login",
                     "/auth/token/refresh"
