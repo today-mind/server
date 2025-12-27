8342639353bde3f08a7175db05ecde72a146e07c
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-27 19:31:23 +0900

    feature(exception): globalExceptionHandler Validation 예외 처리

--- src/main/java/com/example/todaymindserver/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/todaymindserver/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.todaymindserver.common.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,7 +14,38 @@ import lombok.extern.slf4j.Slf4j;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    /** 예상 못한 모든 서버 오류 */
+    /** DTO 필드 검증 실패 오류 */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        log.error("오류 내용: {}", e.getMessage(), e);
+
+        ErrorResponse response = ErrorResponse.builder()
+            .status(400)
+            .code("BAD_REQUEST")
+            .message("잘못된 요청 값 입니다.")
+            .build();
+
+        return ResponseEntity.status(400).body(response);
+    }
+
+    /** 파라미터 누락 오류 */
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+        MissingServletRequestParameterException e) {
+
+        log.error("오류 내용: {}", e.getMessage(), e);
+
+        ErrorResponse response = ErrorResponse.builder()
+            .status(400)
+            .code("BAD_REQUEST")
+            .message("잘못된 요청 값 입니다.")
+            .build();
+
+        return ResponseEntity.status(400).body(response);
+    }
+
+    /** 비즈니스 오류 오류 */
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
 
