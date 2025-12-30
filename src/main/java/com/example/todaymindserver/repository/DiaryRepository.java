0caa2f03c4b2631b57af8334eccde513f3b41c12
kimyongjun0129 <kimyongjun0129@naver.com>
2025-12-31 00:24:33 +0900

    remove(diary): 사용하지 않는 import 문 제거

--- src/main/java/com/example/todaymindserver/repository/DiaryRepository.java
+++ src/main/java/com/example/todaymindserver/repository/DiaryRepository.java
@@ -4,8 +4,6 @@ import com.example.todaymindserver.domain.diary.Diary;
 import com.example.todaymindserver.domain.user.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
