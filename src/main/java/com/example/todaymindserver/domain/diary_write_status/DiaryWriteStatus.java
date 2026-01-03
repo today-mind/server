dcafae453acba7942103803d70dcb09f5e09ebea
kimyongjun0129 <kimyongjun0129@naver.com>
2026-01-04 00:35:49 +0900

    remove(user): 사용하지 않는 import 문 제거

--- src/main/java/com/example/todaymindserver/domain/diary_write_status/DiaryWriteStatus.java
+++ src/main/java/com/example/todaymindserver/domain/diary_write_status/DiaryWriteStatus.java
@@ -1,21 +1,16 @@
 package com.example.todaymindserver.domain.diary_write_status;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import com.example.todaymindserver.domain.BaseTime;
-import com.example.todaymindserver.domain.user.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
